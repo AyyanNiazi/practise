a4f53e7cce4e12bd2fc0382ed7aaa081db5b059f
Ayyan <ayyanniazi82@gmail.com>
2019-02-26 10:20:09 +0500

    second

--- src/component/routes.js
+++ src/component/routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter,Route,Switch} from 'react-router-dom'
 import Login from './login'
 import Home from './home'
+import About from './about'
 class Routes extends React.Component {
     constructor(props) {
         super(props);
@@ -13,6 +14,7 @@ class Routes extends React.Component {
                 <BrowserRouter>
                     <Switch> 
                     <Route exact path='/' component={Login} />
+                    <Route path='/about' component={About} />
                     <Route path='/home' component={Home} />
                     </Switch>
                 </BrowserRouter>
